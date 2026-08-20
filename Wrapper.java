27e785839ac33b9cf23604acd9cde95ebbae43d7
ChirraHasini <chirrahasini3438@gmail.com>
2026-08-20 10:44:04 +0000

    added

--- Wrapper.java
+++ Wrapper.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 class Wrapper{
     public static void main(String args[])
     {
-        Character ch = 'a';
-        char c = ch;
-        ArrayList<Integer>List = new ArrayList<>();
+         ArrayList<Integer>List = new ArrayList<>();
         List.add(24);
         int num = List.get(0);
         System.out.println(num);
--- writeCharacterTest.java
+++ writeCharacterTest.java
@@ -1,4 +1,4 @@
-import java.io.*;
+
 class writeCharacterTest{
     public static void main(String args[]){
         int byteval;
